6c56028a317b54b6a8578d1ad77b1d4e483d9daf
amie348 <ahmadyaqoob89@gmail.com>
2022-05-23 02:06:39 +0500

    deplouing on heroku

---
+++ utils/credentials.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    googleAppRefreshToken : `1//04uggzr4BKVXOCgYIARAAGAQSNgF-L9IrIndjtQTn1ogrLJnp8uI8V5SoS2C0sTOGqPFUpMSVe2EHrw0TJPsBub3AWs3hLhfzBQ`,
+    googleAppClientId :`781313282863-vf842v51f8aamfo6kdjfs1ac17hoshfc.apps.googleusercontent.com`,
+    googleAppClientSecret: `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`,
+    senderEmail: `ahmadyaqoob89@gmail.com`
+
+}
--- utils/email.js
+++ utils/email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const {google} = require('googleapis')
 const {OAuth2} = google.auth;
 const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground'
-const {sendGridApiKey, googleAppRefreshToken, googleAppClientId, googleAppClientSecret, senderEmail} = require(`./credentials.js`);
+const { googleAppRefreshToken, googleAppClientId, googleAppClientSecret, senderEmail} = require(`./credentials.js`);
 
 const oauth2Client = new OAuth2 (
     googleAppClientId,
