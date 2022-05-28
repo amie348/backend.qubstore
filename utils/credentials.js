235bb5a214b9bf1d93bbabad69eaf374da28e5e1
amie348 <ahmadyaqoob89@gmail.com>
2022-05-28 21:12:18 +0500

    credentials file deleted

--- utils/credentials.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    googleAppRefreshToken : `1//04n4QTmZ36Lc6CgYIARAAGAQSNwF-L9Irn5ZS1du0AxKak2a9QEXGb_edBu5SUno9mVJ35ndfYAiL0XzEkxlA66qfEmiO-1e57Cw`,
-    googleAppClientId :`568284619595-r8s74ea6l0fdf37hcni8hut17p59emhn.apps.googleusercontent.com`,
-    googleAppClientSecret: `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`,
-    senderEmail: `info@qubstore.com`
-
-}
